7490efe21b3ccf6ea6c2b8d7f7477762325e9f59
Edward <edward@enclave.vn>
2024-09-18 21:17:47 +0700

    Initial Commit

---
+++ EasyPenNote/EasyPenNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPenNoteApp.swift
+//  EasyPenNote
+//
+//  Created by Edward Nguyen on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyPenNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
